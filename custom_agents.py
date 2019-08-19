9d9862cbeca78afd243dbbd51e705a994601cd2b
Raghu Rajan <raghuspacerajan@gmail.com>
2019-08-19 20:56:43 +0200

    Writing experiments info to stats file

--- custom_agents.py
+++ custom_agents.py
@@ -7,7 +7,6 @@ import numpy as np
 from ray.rllib.agents.trainer import Trainer, with_common_config
 from ray.rllib.utils.annotations import override
 
-
 # yapf: disable
 # __sphinx_doc_begin__
 class RandomAgent(Trainer):
@@ -82,9 +81,10 @@ class VIAgent(Trainer):
                 max_diff = np.max(np.absolute(self.V_old - self.V))
         # import time
         # time.sleep(2)
-        for s in range(state_space_size):
-            print("FINAL self.V[:]", s, max_diff, self.V[:], [self.env.R(s, a) for a in range(self.env.action_space.n)])
+#         for s in range(state_space_size):
+#             print("FINAL self.V[:]", s, max_diff, self.V[:], [self.env.R(s, a) for a in range(self.env.action_space.n)])
 
+        print("Total iterations:", total_iterations)
         rewards = []
         steps = 0
         for _ in range(self.config["rollouts_per_iteration"]):
@@ -103,20 +103,6 @@ class VIAgent(Trainer):
             "timesteps_this_iter": steps,
         }
 
-# __sphinx_doc_end__
-# don't enable yapf after, it's buggy here
-
-
-# def _import_random_agent():
-#     # from ray.rllib.contrib.random_agent.random_agent import RandomAgent
-#     return RandomAgent
-#
-# from ray.rllib.agents.registry import CONTRIBUTED_ALGORITHMS
-#
-# CONTRIBUTED_ALGORITHMS["contrib/RandomAgent"] = _import_random_agent,
-
-
-
 import ray
 from ray import tune
 from ray.rllib.utils.seed import seed as rllib_seed
@@ -127,6 +113,67 @@ register_env("RLToy-v0", lambda config: RLToyEnv(config))
 
 # rllib_seed(0, 0, 0)
 ray.init()
+
+state_space_sizes = [2**i for i in range(4,6)]
+action_space_sizes = [2**i for i in range(1,6)]
+delays = [0] + [2**i for i in range(5)]
+sequence_lengths = [i for i in range(1,6)]
+reward_densities = [0.25] # np.linspace(0.0, 1.0, num=5)
+# make_reward_dense = [True, False]
+terminal_state_densities = [0.25] # np.linspace(0.1, 1.0, num=5)
+algorithms = ["DQN"]
+
+print(algorithms, state_space_sizes, action_space_sizes, delays, sequence_lengths, reward_densities, terminal_state_densities)
+
+print('# Algorithm, state_space_size, action_space_size, delay, sequence_length, reward_density,'
+               'terminal_state_density ')
+
+
+# stats = {}
+# aaaa = 3
+
+#TODO Write addnl. line at beginning of file for column names
+# fout = open('rl_stats_temp.csv', 'a') #hardcoded
+# fout.write('# basename, n_points, n_features, n_trees ')
+
+import time
+start = time.time()
+print(algorithms, state_space_sizes, action_space_sizes, delays,
+      sequence_lengths, reward_densities, terminal_state_densities)
+
+
+def on_train_result(info):
+#     print("#############trainer.train() result: {} -> {} episodes".format(
+#         info["trainer"], info["result"]["episodes_this_iter"]), info)
+    # you can mutate the result dict to add new fields to return
+#     stats['episode_len_mean'] = info['result']['episode_len_mean']
+#     print("++++++++", aaaa, stats)
+    algorithm = info["trainer"]._name
+    state_space_size = info["result"]["config"]["env_config"]["state_space_size"]
+    action_space_size = info["result"]["config"]["env_config"]["action_space_size"]
+    delay = info["result"]["config"]["env_config"]["delay"]
+    sequence_length = info["result"]["config"]["env_config"]["sequence_length"]
+    reward_density = info["result"]["config"]["env_config"]["reward_density"]
+    terminal_state_density = info["result"]["config"]["env_config"]["terminal_state_density"]
+
+    timesteps_total = info["result"]["timesteps_total"] # also has episodes_total and training_iteration
+    episode_reward_mean = info["result"]["episode_reward_mean"] # also has max and min
+    episode_len_mean = info["result"]["episode_len_mean"]
+
+    fout = open('/home/rajanr/custom-gym-env/rl_stats_temp.csv', 'a') #hardcoded
+    fout.write('# Algorithm, state_space_size, action_space_size, delay, sequence_length, reward_density, '
+               'terminal_state_density, ' + str(algorithm) + ' ' + str(state_space_size) +
+               ' ' + str(action_space_size) + ' ' + str(delay) + ' ' + str(sequence_length)
+               + ' ' + str(reward_density) + ' ' + str(terminal_state_density) + '\n')
+               # Writes every iteration, would slow things down. #hack
+    fout.write(str(timesteps_total) + ' ' + str(episode_reward_mean) +
+               ' ' + str(episode_len_mean) + '\n')
+    fout.close()
+
+    info["result"]["callback_ok"] = True
+
+
+
 # tune.run(
 #     RandomAgent,
 #     stop={
@@ -173,56 +220,66 @@ ray.init()
 #     },
 # )
 
+for algorithm in algorithms: #TODO each one has different config_spaces
+    for state_space_size in state_space_sizes:
+        for action_space_size in action_space_sizes:
+            for delay in delays:
+                for sequence_length in sequence_lengths:
+                    for reward_density in reward_densities:
+                        for terminal_state_density in terminal_state_densities:
 
-tune.run(
-    "DQN",
-    stop={
-        "timesteps_total": 20000,
-          },
-    config={
-      "adam_epsilon": 0.00015,
-      "beta_annealing_fraction": 1.0,
-      "buffer_size": 1000000,
-      "double_q": False,
-      "dueling": False,
-      "env": "RLToy-v0",
-      "env_config": {
-        'state_space_type': 'discrete',
-        'action_space_type': 'discrete',
-        'state_space_size': 16,
-        'action_space_size': 16,
-        'generate_random_mdp': True,
-        'delay': 6,
-        'sequence_length': 1,
-        'reward_density': 0.25,
-        'terminal_state_density': 0.25
-        },
-      "exploration_final_eps": 0.01,
-      "exploration_fraction": 0.1,
-      "final_prioritized_replay_beta": 1.0,
-      "hiddens": [
-        256
-      ],
-      "learning_starts": 2000,
-      "lr": 6.25e-05, # "lr": grid_search([1e-2, 1e-4, 1e-6]),
-      "n_step": 1,
-      "noisy": False,
-      "num_atoms": 1,
-      "prioritized_replay": False,
-      "prioritized_replay_alpha": 0.5,
-      "sample_batch_size": 4,
-      "schedule_max_timesteps": 20000,
-      "target_network_update_freq": 80,
-      "timesteps_per_iteration": 100,
-      "train_batch_size": 32
-    },
-)
+                            tune.run(
+                                algorithm,
+                                stop={
+                                    "timesteps_total": 20000,
+                                      },
+                                config={
+                                  "adam_epsilon": 0.00015,
+                                  "beta_annealing_fraction": 1.0,
+                                  "buffer_size": 1000000,
+                                  "double_q": False,
+                                  "dueling": False,
+                                  "env": "RLToy-v0",
+                                  "env_config": {
+                                    'state_space_type': 'discrete',
+                                    'action_space_type': 'discrete',
+                                    'state_space_size': state_space_size,
+                                    'action_space_size': action_space_size,
+                                    'generate_random_mdp': True,
+                                    'delay': delay,
+                                    'sequence_length': sequence_length,
+                                    'reward_density': reward_density,
+                                    'terminal_state_density': terminal_state_density
+                                    },
+                                  "exploration_final_eps": 0.01,
+                                  "exploration_fraction": 0.1,
+                                  "final_prioritized_replay_beta": 1.0,
+                                  "hiddens": [
+                                    256
+                                  ],
+                                  "learning_starts": 2000,
+                                  "lr": 6.25e-05, # "lr": grid_search([1e-2, 1e-4, 1e-6]),
+                                  "n_step": 1,
+                                  "noisy": False,
+                                  "num_atoms": 1,
+                                  "prioritized_replay": False,
+                                  "prioritized_replay_alpha": 0.5,
+                                  "sample_batch_size": 4,
+                                  "schedule_max_timesteps": 20000,
+                                  "target_network_update_freq": 80,
+                                  "timesteps_per_iteration": 100,
+                                  "train_batch_size": 32,
 
+                                          "callbacks": {
+                            #                 "on_episode_start": tune.function(on_episode_start),
+                            #                 "on_episode_step": tune.function(on_episode_step),
+                            #                 "on_episode_end": tune.function(on_episode_end),
+                            #                 "on_sample_end": tune.function(on_sample_end),
+                                            "on_train_result": tune.function(on_train_result),
+                            #                 "on_postprocess_traj": tune.function(on_postprocess_traj),
+                                        },
+                                },
+                            )
 
-
-if __name__ == "__main__":
-    trainer = RandomAgent(
-        env="CartPole-v0", config={"rollouts_per_iteration": 10})
-    result = trainer.train()
-    assert result["episode_reward_mean"] > 10, result
-    print("Test: OK")
+end = time.time()
+print("No. of seconds to run:", end - start)
