f20b2ce5a121a5eee022f562aba971bd2f704992
RaghuRajan <15613406+RaghuSpaceRajan@users.noreply.github.com>
2019-09-07 22:47:40 +0200

    113 again: rainbow still taking too much memory. Hacked and split it into multiple scripts!

--- temp_run.sh
+++ temp_run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 pwd
-date >>temp.txt
-# 103 Relaunch of 70; python custom_agents_noises.py 103 | tee 103.log
-python custom_agents.py 106 | tee 106.log # rerun of 26
-date >>temp.txt
-python custom_agents_rainbow_noises.py 110 | tee 110.log # 
-date >>temp.txt
+# date >>temp.txt
+# # 103 Relaunch of 70; python custom_agents_noises.py 103 | tee 103.log
+# python custom_agents.py 106 | tee 106.log # rerun of 26
+# date >>temp.txt
+# python custom_agents_rainbow_noises.py 110 | tee 110.log #
+# date >>temp.txt
+
+
+python custom_agents_rainbow_noises.py 113 | tee -a 113.log
+python custom_agents_rainbow_noises1.py 113 | tee -a 113.log
+python custom_agents_rainbow_noises2.py 113 | tee -a 113.log
+python custom_agents_rainbow_noises3.py 113 | tee -a 113.log
+python custom_agents_rainbow_noises4.py 113 | tee -a 113.log
