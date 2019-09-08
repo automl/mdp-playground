501462079cb0ea2336f6c0a88736fdc545b29580
Raghu Rajan <raghuspacerajan@gmail.com>
2019-09-08 14:25:27 +0200

    Bash apparently reloads every line of the script from disk every time it runs a new line. Checking in an extra bash script to combat that.

---
+++ temp_run_2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pwd
+# date >>temp.txt
+# # 103 Relaunch of 70; python custom_agents_noises.py 103 | tee 103.log
+# python custom_agents.py 106 | tee 106.log # rerun of 26
+# date >>temp.txt
+# python custom_agents_rainbow_noises.py 110 | tee 110.log #
+# date >>temp.txt
+
+
+python custom_agents_rainbow.py 123 | tee -a 123.log
+python custom_agents_rainbow1.py 123 | tee -a 123.log
+python custom_agents_rainbow2.py 123 | tee -a 123.log
+python custom_agents_rainbow3.py 123 | tee -a 123.log
+python custom_agents_rainbow4.py 123 | tee -a 123.log
+
+
+# python custom_agents_rainbow_noises.py 122 | tee -a 122.log
+# python custom_agents_rainbow_noises1.py 122 | tee -a 122.log
+# python custom_agents_rainbow_noises2.py 122 | tee -a 122.log
+# python custom_agents_rainbow_noises3.py 122 | tee -a 122.log
+# python custom_agents_rainbow_noises4.py 122 | tee -a 122.log
+
+
+
+# python custom_agents_a3c_noises.py 115 | tee -a 115.log
+# python custom_agents_a3c_lstm_noises.py 116 | tee -a 116.log
+#
+# python custom_agents_sparsity.py 117 | tee -a 117.log
+# python custom_agents_a3c_sparsity.py 118 | tee -a 118.log
+# python custom_agents_a3c_lstm_sparsity.py 119 | tee -a 119.log
+# python custom_agents_rainbow_sparsity.py 120 | tee -a 120.log
