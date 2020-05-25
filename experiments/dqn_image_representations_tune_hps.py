a12d9bf719d303eed1d9cebe717bd3e9de0f86a3
Raghu Rajan <raghuspacerajan@gmail.com>
2020-05-25 14:36:34 +0200

    Added tune HPs for DQN image reps.

--- experiments/dqn_image_representations_tune_hps.py
+++ experiments/dqn_image_representations_tune_hps.py
@@ -1,6 +1,21 @@
+'''###IMP dummy_seed should always be last in the order in the OrderedDict below!!!
 '''
-'''
-num_seeds = 3
+num_seeds = 10
+
+import itertools
+transforms = ['shift', 'scale', 'flip', 'rotate']
+image_transforms = []
+for i in range(len(transforms) + 1):
+    curr_combos = list(itertools.combinations(transforms, i))
+    for j in range(len(curr_combos)):
+        if i == 0:
+            curr_elem = 'none' # this is written to a CSV file with ' ' separater, therefore it needs to have some value in there.
+        else:
+            curr_elem = ''
+        for k in range(i):
+            curr_elem += curr_combos[j][k] + ','
+        # print(curr_elem, i, j)
+        image_transforms.append(curr_elem)
 
 from collections import OrderedDict
 var_env_configs = OrderedDict({
@@ -14,23 +29,17 @@ var_env_configs = OrderedDict({
     'transition_noise': [0],#, 0.01, 0.02, 0.10, 0.25]
     'reward_noise': [0],#, 1, 5, 10, 25] # Std dev. of normal dist.
     'image_representations': [True],
-    'image_transforms': ['shift', 'scale'], # , 'flip', 'rotate', 'shift,scale,rotate,flip']
+    'image_transforms': ['none'], #image_transforms, # ['shift', 'scale', 'flip', 'rotate', 'shift,scale,rotate,flip']
     'image_width': [100],
     'image_height': [100],
     'dummy_seed': [i for i in range(num_seeds)],
 })
 
 var_agent_configs = OrderedDict({
-    # Learning rate
-    "lr": [1e-3, 1e-4, 1e-5], #
-    # Learning rate schedule
-    "lr_schedule": [None],
-    # GAE(gamma) parameter
-    "lambda": [0.0, 0.5, 0.95, 1.0], #
-    # Value Function Loss coefficient
-    "vf_loss_coeff": [0.5], #[0.1, 0.5, 2.5]
-    # Entropy coefficient
-    "entropy_coeff": [0.1], #[0.001, 0.01, 0.1, 1]
+    "learning_starts": [500, 1000, 2000],
+    "lr": [1e-3, 1e-4, 1e-5], # "lr": grid_search([1e-2, 1e-4, 1e-6]),
+    "n_step": [1],
+    "target_network_update_freq": [8, 80, 800],
 })
 
 
@@ -83,6 +92,7 @@ var_configs = OrderedDict({
 "model": var_model_configs,
 })
 
+
 env_config = {
     "env": "RLToy-v0",
     "horizon": 100,
@@ -97,33 +107,32 @@ env_config = {
     },
 }
 
-algorithm = "A3C"
+algorithm = "DQN"
 agent_config = {
-    # Size of rollout batch
-    "sample_batch_size": 10, # maybe num_workers * sample_batch_size * num_envs_per_worker * grads_per_step
-    "train_batch_size": 100, # seems to have no effect
-    # Use PyTorch as backend - no LSTM support
-    "use_pytorch": False,
-    # Max global norm for each gradient calculated by worker
-    "grad_clip": 10.0, # low prio.
-    # Min time per iteration
+    "adam_epsilon": 1e-4,
+    "beta_annealing_fraction": 1.0,
+    "buffer_size": 1000000,
+    "double_q": False,
+    "dueling": False,
+    "exploration_final_eps": 0.01,
+    "exploration_fraction": 0.1,
+    "final_prioritized_replay_beta": 1.0,
+    "hiddens": None,
+    "noisy": False,
+    "num_atoms": 1,
+    "prioritized_replay": False,
+    "prioritized_replay_alpha": 0.5,
+    "sample_batch_size": 4, # Renamed from sample_batch_size in some Ray version
+    "schedule_max_timesteps": 20000,
+    "timesteps_per_iteration": 1000,
     "min_iter_time_s": 0,
-    # Workers sample async. Note that this increases the effective
-    # sample_batch_size by up to 5x due to async buffering of batches.
-    "sample_async": True,
-    "timesteps_per_iteration": 7500,
-    "num_workers": 3,
-    "num_envs_per_worker": 5,
-
-    "optimizer": {
-        "grads_per_step": 10
-    },
+    "train_batch_size": 32,
 }
 
 
 model_config = {
     "model": {
-        "fcnet_hiddens": [[128, 128, 128]],
+        "fcnet_hiddens": [256, 256],
         # "custom_preprocessor": "ohe",
         "custom_options": {},  # extra options to pass to your preprocessor
         "conv_activation": "relu",
