f27e207db04d86a7281ceeb946e9f6dbd1ad1cd7
Raghu Rajan <raghuspacerajan@gmail.com>
2020-08-13 18:57:05 +0200

    # 232 DQN sparsity 50 states

---
+++ experiments/dqn_sparsity_50_states.py
@@ -0,0 +1,69 @@
+num_seeds = 10
+from collections import OrderedDict
+var_env_configs = OrderedDict({
+    'state_space_size': [50],#, 10, 12, 14] # [2**i for i in range(1,6)]
+    'action_space_size': [50],#2, 4, 8, 16] # [2**i for i in range(1,6)]
+    'delay': [0],
+    'sequence_length': [1],#i for i in range(1,4)]
+    'reward_density': [0.25, 0.5, 0.75], # np.linspace(0.0, 1.0, num=5)
+    'make_denser': [False],
+    'terminal_state_density': [0.25], # np.linspace(0.1, 1.0, num=5)
+    'transition_noise': [0],#, 0.01, 0.02, 0.10, 0.25]
+    'reward_noise': [0],#, 1, 5, 10, 25] # Std dev. of normal dist.
+    'dummy_seed': [i for i in range(num_seeds)],
+})
+
+var_configs = OrderedDict({
+"env": var_env_configs
+})
+
+env_config = {
+    "env": "RLToy-v0",
+    "env_config": {
+        'seed': 0, #seed
+        'state_space_type': 'discrete',
+        'action_space_type': 'discrete',
+        'generate_random_mdp': True,
+        'repeats_in_sequences': False,
+        'reward_scale': 1.0,
+        'completely_connected': True,
+    },
+}
+
+algorithm = "DQN"
+agent_config = {
+    "adam_epsilon": 1e-4,
+    "beta_annealing_fraction": 1.0,
+    "buffer_size": 1000000,
+    "double_q": False,
+    "dueling": False,
+    "exploration_final_eps": 0.01,
+    "exploration_fraction": 0.1,
+    "final_prioritized_replay_beta": 1.0,
+    "hiddens": None,
+    "learning_starts": 1000,
+    "lr": 1e-4, # "lr": grid_search([1e-2, 1e-4, 1e-6]),
+    "n_step": 1,
+    "noisy": False,
+    "num_atoms": 1,
+    "prioritized_replay": False,
+    "prioritized_replay_alpha": 0.5,
+    "sample_batch_size": 4,
+    "schedule_max_timesteps": 20000,
+    "target_network_update_freq": 800,
+    "timesteps_per_iteration": 1000,
+    "train_batch_size": 32,
+}
+
+model_config = {
+    "model": {
+        "fcnet_hiddens": [256, 256],
+        "custom_preprocessor": "ohe",
+        "custom_options": {},  # extra options to pass to your preprocessor
+        "fcnet_activation": "tanh",
+        "use_lstm": False,
+        "max_seq_len": 20,
+        "lstm_cell_size": 256,
+        "lstm_use_prev_action_reward": False,
+    },
+}
