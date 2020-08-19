30c38de2f5a60e86da20bf1937869d64cca6d053
Raghu Rajan <raghuspacerajan@gmail.com>
2020-08-19 20:27:01 +0200

    # 234 SAC on HalfCheetah-v3

--- experiments/sac_halfcheetah_action_max.py
+++ experiments/sac_halfcheetah_action_max.py
@@ -22,8 +22,6 @@ env_config = {
 
 algorithm = "SAC"
 agent_config = {
-    # Initial value to use for the entropy weight alpha.
-    "initial_alpha": 0.1,
     "optimization": {
         "actor_learning_rate": 3e-4,
         "critic_learning_rate": 3e-4,
@@ -34,10 +32,6 @@ agent_config = {
     # How many steps of the model to sample before learning starts.
     "learning_starts": 10000,
 
-    # Apply a state preprocessor with spec given by the "model" config option
-    # (like other RL algorithms). This is mostly useful if you have a weird
-    # observation shape, like an image. Disabled by default.
-    "use_state_preprocessor": False,
     # N-step Q learning
     "n_step": 1,
     # Update the target network every `target_network_update_freq` steps.
@@ -48,16 +42,6 @@ agent_config = {
 
     # If True prioritized replay buffer will be used.
     "prioritized_replay": True,
-    # Alpha parameter for prioritized replay buffer.
-    "prioritized_replay_alpha": 0.6,
-    # Beta parameter for sampling from prioritized replay buffer.
-    "prioritized_replay_beta": 0.4,
-    # Time steps over which the beta parameter is annealed.
-    # "prioritized_replay_beta_annealing_timesteps": 20000,
-    # Final value of beta
-    "final_prioritized_replay_beta": 0.4,
-    # Epsilon to add to the TD errors when updating priorities.
-    "prioritized_replay_eps": 1e-6,
 
     # "schedule_max_timesteps": 20000,
     "timesteps_per_iteration": 1000,
@@ -87,12 +71,4 @@ model_config = {
         "fcnet_hiddens": [256, 256],
     },
 
-    "model": {
-        # "custom_preprocessor": "ohe",
-        "custom_options": {},  # extra options to pass to your preprocessor
-        # "no_final_linear": False,
-        # "vf_share_layers": True,
-        # "fcnet_activation": "tanh",
-        "use_lstm": False,
-    },
 }
