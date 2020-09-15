8ded4d88927ce67cc5cdaa344d770710dd25a559
Raghu Rajan <raghuspacerajan@gmail.com>
2020-09-15 14:20:59 +0200

    # 260, 261 TD3 tune HPs Reacher, Pusher

---
+++ experiments/td3_reacher_tune_hps.py
@@ -0,0 +1,91 @@
+'''###IMP dummy_seed should always be last in the order in the OrderedDict below!!!
+'''
+num_seeds = 3
+
+from collections import OrderedDict
+var_env_configs = OrderedDict({
+    'dummy_seed': [i for i in range(num_seeds)],
+})
+
+var_agent_configs = OrderedDict({
+    "policy_delay": [2, 3, 4],
+    # N-step Q learning
+    "n_step": [2, 3, 4],
+    "target_noise": [0.02, 0.2, 2.0],
+    "target_noise_clip": [1.5, 2.5], # this is abs. val. for our purposes we need relative so we hack that in run_experiments.py
+})
+
+# var_model_configs = OrderedDict({
+# })
+
+var_configs = OrderedDict({
+"env": var_env_configs,
+"agent": var_agent_configs,
+# "model": var_model_configs,
+})
+
+env_config = {
+    "env": "ReacherWrapper-v2",
+    "horizon": 100,
+    "env_config": {
+    },
+}
+
+algorithm = "TD3"
+agent_config = {
+    # Learning rate for the critic (Q-function) optimizer.
+    "critic_lr": 1e-3,
+    # Learning rate for the actor (policy) optimizer.
+    "actor_lr": 1e-3,
+    # Update the target by \tau * policy + (1-\tau) * target_policy
+    "tau": 0.002,
+    # How many steps of the model to sample before learning starts.
+    "learning_starts": 10000,
+
+    "critic_hiddens": [256, 256],
+    "actor_hiddens": [256, 256],
+
+    "twin_q": True,
+    "smooth_target_policy": True,
+
+    # Update the target network every `target_network_update_freq` steps.
+#    "target_network_update_freq": 0,
+
+    "buffer_size": 1000000,
+
+    # If True prioritized replay buffer will be used.
+    "prioritized_replay": False,
+
+    # "schedule_max_timesteps": 20000,
+    "timesteps_per_iteration": 1000,
+    # Update the replay buffer with this many samples at once. Note that this
+    # setting applies per-worker if num_workers > 1.
+    # "rollout_fragment_length": 1,
+    "rollout_fragment_length": 1, # Renamed from sample_batch_size in some Ray version
+    "train_batch_size": 256,
+    "min_iter_time_s": 0,
+    "num_workers": 0,
+    "num_gpus": 0,
+#    "evaluation_interval": 1,
+
+}
+
+
+model_config = {
+}
+
+from ray import tune
+eval_config = {
+    "evaluation_interval": 1, # I think this means every x training_iterations
+    "evaluation_config": {
+        "explore": False,
+        "evaluation_num_episodes": 10,
+        "horizon": 100,
+        "env_config": {
+            "dummy_eval": True, #hack Used to check if we are in evaluation mode or training mode inside Ray callback on_episode_end() to be able to write eval stats
+            'transition_noise': 0 if "state_space_type" in env_config["env_config"] and env_config["env_config"]["state_space_type"] == "discrete" else tune.function(lambda a: a.normal(0, 0)),
+            'reward_noise': tune.function(lambda a: a.normal(0, 0)),
+            'action_loss_weight': 0.0,
+        }
+    },
+}
--- run_experiments_on_cluster_nemo.sh
+++ run_experiments_on_cluster_nemo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #MOAB -N mdpp
-#MOAB -t 0-49 # specifies array job indices
+#MOAB -t 0-161 # specifies array job indices
 #MOAB -l nodes=1:ppn=2
-#MOAB -l walltime=0:08:00:00
+#MOAB -l walltime=0:06:00:00
 #MOAB -l pmem=8GB
 #MOAB -d /work/ws/nemo/fr_rr1034-ws_mdpp-0 # initial working dir.
 ##MOAB -V # export env. variables from launch env. I think
@@ -22,7 +22,7 @@ echo "TMPDIR: " $TMPDIR
 
 printenv
 
-export EXP_NAME='sac_pusher_time_unit' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
+export EXP_NAME='td3_reacher_tune_hps' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
 
 echo -e '\033[32m'
 # Print some information about the job to STDOUT
