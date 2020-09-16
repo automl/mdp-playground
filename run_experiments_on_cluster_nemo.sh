69a236db77b978eea7a3d8d7d51d2cfa056157b9
Raghu Rajan <raghuspacerajan@gmail.com>
2020-09-16 13:33:51 +0200

    # 258, 259 actual

--- run_experiments_on_cluster_nemo.sh
+++ run_experiments_on_cluster_nemo.sh
@@ -2,7 +2,7 @@
 #MOAB -N mdpp
 #MOAB -t 0-49 # specifies array job indices
 #MOAB -l nodes=1:ppn=2
-#MOAB -l walltime=0:07:00:00
+#MOAB -l walltime=0:16:00:00
 #MOAB -l pmem=8GB
 #MOAB -d /work/ws/nemo/fr_rr1034-ws_mdpp-0 # initial working dir.
 ##MOAB -V # export env. variables from launch env. I think
@@ -22,7 +22,7 @@ echo "TMPDIR: " $TMPDIR
 
 printenv
 
-export EXP_NAME='ddpg_pusher_time_unit' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
+export EXP_NAME='sac_pusher_time_unit' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
 
 echo -e '\033[32m'
 # Print some information about the job to STDOUT
