0d58bc14f530c3cb3ab9b89c6d1958bed4363a3f
Raghu Rajan <raghuspacerajan@gmail.com>
2020-09-18 18:08:54 +0200

    # 269 actual

--- run_experiments_on_cluster_nemo.sh
+++ run_experiments_on_cluster_nemo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #MOAB -N mdpp
-#MOAB -t 0-4 # specifies array job indices
-#MOAB -l nodes=1:ppn=4
-#MOAB -l walltime=0:02:00:00
-#MOAB -l pmem=16GB # Seems like it is memory per CPU core
+#MOAB -t 0-24 # specifies array job indices
+#MOAB -l nodes=1:ppn=2
+#MOAB -l walltime=0:90:00:00
+#MOAB -l pmem=32GB # Seems like it is memory per CPU core
 #MOAB -d /work/ws/nemo/fr_rr1034-ws_mdpp-0 # initial working dir.
 ##MOAB -V # export env. variables from launch env. I think
 ##MOAB -o output_filename
