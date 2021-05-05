8e775354f677538c46f5e5a6edbe997c20917ca9
Raghu Rajan <raghuspacerajan@gmail.com>
2021-05-05 18:18:33 +0200

    # 310 Hydra configs on qbert: test run

--- run_experiments.py
+++ run_experiments.py
@@ -313,24 +313,6 @@ else:
 
 
 
-from functools import reduce
-def deepmerge(a, b, path=None):
-    '''Merges dict b into dict a
-
-    Based on: https://stackoverflow.com/questions/7204805/how-to-merge-dictionaries-of-dictionaries/7205107#7205107
-    '''
-    if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                deepmerge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass # same leaf value
-            else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
-        else:
-            a[key] = b[key]
-    return a
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
