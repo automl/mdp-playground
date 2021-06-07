61c44391e5e5691a25f9ae0221836c5a9941c6c9
Jan Ole von Hartz <hartzj@cs.uni-freiburg.de>
2021-06-07 16:37:52 +0200

    test ignoring docs again

---
+++ publish-docs.sh
@@ -0,0 +1,16 @@
+# based on https://stackoverflow.com/a/40178818/11051330
+
+STATUS="$(git status)"
+DOCSDIR="docs/_build/"
+
+if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+then
+    sed -i "" '$DOCSDIR' ./.gitignore
+    git add .
+    git commit -m "Edit .gitignore to publish docs"
+    git subtree push --prefix $DOCSDIR/html/ origin gh-pages
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "Need clean working directory to publish"
+fi
