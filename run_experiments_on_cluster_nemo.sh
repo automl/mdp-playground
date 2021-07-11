8b9cc83cdd7572d203ae2aa19780c527cd4a3d5f
Raghu Rajan <raghuspacerajan@gmail.com>
2021-07-11 12:10:55 +0200

    # 318 test

--- run_experiments_on_cluster_nemo.sh
+++ run_experiments_on_cluster_nemo.sh
@@ -23,7 +23,7 @@ echo "TMPDIR: " $TMPDIR
 
 printenv
 
-export EXP_NAME='sac_halfcheetah_time_unit_action_max' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
+export EXP_NAME='ddpg_halfcheetah_time_unit_action_max' # Ideally contains Area of research + algorithm + dataset # Could just pass this as job name?
 
 echo -e '\033[32m'
 # Print some information about the job to STDOUT
