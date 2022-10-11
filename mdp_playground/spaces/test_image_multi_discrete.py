1645f4e8ec95a0f307d3af26941d16020de9d683
Raghu Rajan <raghuspacerajan@gmail.com>
2022-10-11 20:46:22 +0200

    Fix failing test

--- mdp_playground/spaces/test_image_multi_discrete.py
+++ mdp_playground/spaces/test_image_multi_discrete.py
@@ -18,6 +18,8 @@ class TestImageMultiDiscrete(unittest.TestCase):
         ds4 = [ds4.n]
         print(ds4)
         imd = ImageMultiDiscrete(ds4, transforms="shift")
+        print("imd.dtype, shape", imd.dtype, imd.shape)
+        print("imd", imd)
         from PIL import Image
 
         # img1 = Image.fromarray(imd.disjoint_states[0][1], 'L')
@@ -25,23 +27,25 @@ class TestImageMultiDiscrete(unittest.TestCase):
         if render:
             img1.show()
 
-        imd = ImageMultiDiscrete(
-            ds4,
-            transforms="scale,shift,rotate,flip",
-            use_custom_images="textures",
-            cust_path="/home/rajanr/textures",
-        )
-        img1 = Image.fromarray(np.squeeze(imd.get_concatenated_image(2)), "RGB")
+        if render:
+            imd = ImageMultiDiscrete(
+                ds4,
+                transforms="scale,shift,rotate,flip",
+                use_custom_images="textures",
+                cust_path="/home/rajanr/textures",
+            )
+            img1 = Image.fromarray(np.squeeze(imd.get_concatenated_image(2)), "RGB")
         if render:
             img1.show()
 
-        imd = ImageMultiDiscrete(
-            ds4,
-            transforms="scale,shift,rotate,flip",
-            use_custom_images="images",
-            cust_path="/home/rajanr/textures",
-        )
-        img1 = Image.fromarray(np.squeeze(imd.get_concatenated_image(1)), "RGB")
+        if render:
+            imd = ImageMultiDiscrete(
+                ds4,
+                transforms="scale,shift,rotate,flip",
+                use_custom_images="images",
+                cust_path="/home/rajanr/textures",
+            )
+            img1 = Image.fromarray(np.squeeze(imd.get_concatenated_image(1)), "RGB")
         if render:
             img1.show()
 
